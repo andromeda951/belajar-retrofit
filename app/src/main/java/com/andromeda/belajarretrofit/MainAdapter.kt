7ab67df94646bce32e57e655ab61ac672e0438f4
Andromeda <andromeda951@gmail.com>
2023-10-13 16:46:10 +0700

    Displays image on RecyclerView

--- app/src/main/java/com/andromeda/belajarretrofit/MainAdapter.kt
+++ app/src/main/java/com/andromeda/belajarretrofit/MainAdapter.kt
@@ -1,14 +1,18 @@
 package com.andromeda.belajarretrofit
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class MainAdapter(private val results: ArrayList<MainModel.Result>) : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
     class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.text_view)
+        val imageView: ImageView = view.findViewById(R.id.image_view)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -20,6 +24,15 @@ class MainAdapter(private val results: ArrayList<MainModel.Result>) : RecyclerVi
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val result = results[position]
         holder.textView.text = result.title
+
+        Log.d("MainAdapter", "resultImage: ${result.image}")
+        Glide.with(holder.imageView)
+            .load(result.image)
+            .placeholder(R.drawable.img_placeholder)
+            .error(R.drawable.img_placeholder)
+            .centerCrop()
+            .into(holder.imageView)
+
     }
 
     override fun getItemCount(): Int {
